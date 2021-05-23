659da43687c20dd1f55bdc37e4b47954e966bac6
stefzouille <contact.lokcy@gmail.com>
2021-05-23 22:32:54 +0200

    add

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -1,4 +1,4 @@
-import { v4 as uuidv4 } from "uuid"
+// import { v4 as uuidv4 } from "uuid"
 import { useDarkMode } from "../context/DarkModeContext"
 import { useTodosDispatch } from "../context/TodosDispatchContext"
 
